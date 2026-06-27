321c7a4faf1c23b46b2439fc976a30f77b42cd9f
Vinicius Guarnieri <svini0966@gmail.com>
2026-06-26 23:49:45 -0300

    feat: adicionar rate limit de 100 req/min global

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ThrottlerModule.forRoot({ throttlers: [{ ttl: 60000, limit: 100 }] }),
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
