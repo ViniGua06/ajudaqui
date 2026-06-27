506169bb317aee41d03f62d5996235b687809b15
Vinicius Guarnieri <svini0966@gmail.com>
2026-06-26 23:30:58 -0300

    feat: adicona swagger no projeto e seta prefixo global das rotas para 'api'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('AjudAqui API')
+    .setDescription('')
+    .setVersion('2.0')
+    .addTag('MAP')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  app.setGlobalPrefix('api');
+  SwaggerModule.setup('api/swagger', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
