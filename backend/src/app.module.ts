8fdade472d84b97cab8f1ece7cc34b1215788e14
Vinicius Guarnieri <svini0966@gmail.com>
2026-06-29 12:44:01 -0300

    feat: adiciona módulo de envio de emails

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,21 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/module';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
   imports: [
     UserModule,
     ThrottlerModule.forRoot({ throttlers: [{ ttl: 60000, limit: 100 }] }),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        auth: {
+          user: process.env.EMAIL_USER,
+          password: process.env.EMAIL_PASSWORD,
+        },
+      },
+    }),
   ],
   providers: [
     {
